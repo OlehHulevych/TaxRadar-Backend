65f25f3989f2d1199e1ce54286df18dbf39babed
Oleh_Hulevych <olegguelvic8@gmail.com>
2026-09-09 13:07:29 +0200

    added global expcptional middleware

---
+++ TaxRadar-Api/Hanlders/GlobalExceptionHandler.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using TaxRadar_Application.Exceptions;
+
+namespace TaxRadar_backned.Hanlders;
+
+public class GlobalExceptionHandler(ILogger<GlobalExceptionHandler> logger)
+    : IExceptionHandler
+{
+  
+
+
+    public async ValueTask<bool> TryHandleAsync(HttpContext httpContext, Exception exception, CancellationToken cancellationToken)
+    {
+        logger.LogError(exception, "Unhandled error occured");
+        httpContext.Response.StatusCode = exception switch
+        {
+            NotFoundException => StatusCodes.Status404NotFound,
+            ValidationException => StatusCodes.Status422UnprocessableEntity,
+            ForbiddenAccessException => StatusCodes.Status403Forbidden,
+            BadRequestException => StatusCodes.Status400BadRequest,
+            _ => StatusCodes.Status500InternalServerError
+
+
+        };
+        await httpContext.Response.WriteAsJsonAsync(new ProblemDetails
+        {
+            Type = exception.GetType().Name,
+            Title = "An error occured",
+            Detail = exception.Message
+        });
+        return true;
+    }
+}
--- TaxRadar-Api/Program.cs
+++ TaxRadar-Api/Program.cs
@@ -1,16 +1,21 @@
 using Scalar.AspNetCore;
 using TaxRadar_Application;
+using TaxRadar_backned.Hanlders;
 using TaxRadar_Infrastructure;
 
 var builder = WebApplication.CreateBuilder(args);
 
 
 builder.Services.AddOpenApi();
+builder.Services.AddExceptionHandler<GlobalExceptionHandler>();
 builder.Services.AddControllers();
 builder.Services.AddInfrastructure(builder.Configuration);
 builder.Services.AddApplication();
+builder.Services.AddProblemDetails();
+
 var app = builder.Build();
 
+app.UseExceptionHandler();
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
