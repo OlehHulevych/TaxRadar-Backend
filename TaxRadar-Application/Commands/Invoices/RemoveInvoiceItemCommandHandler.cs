2cb530f2ee83b51b4f02678c2b92c0295fb0826f
Oleh_Hulevych <oleggulevic8@gmail.com>
2026-09-11 11:17:24 +0200

    add command hadnler for deleting invoice item from existing invoice

---
+++ TaxRadar-Application/Commands/Invoices/RemoveInvoiceItemCommandHandler.cs
@@ -0,0 +1,14 @@
+﻿using MediatR;
+using TaxRadar_Application.Interfaces;
+using TaxRadar_Application.Queries.Invoices;
+
+namespace TaxRadar_Application.Commands.Invoices;
+
+public class RemoveInvoiceItemCommandHandler(IInvoiceRepository repository):IRequestHandler<RemoveInvoiceItemQuery>
+{
+    public async Task Handle(RemoveInvoiceItemQuery request, CancellationToken cancellationToken)
+    {
+        await repository.RemoveItemFromInvoice(request.InvoiceId, request.ItemId, cancellationToken);
+        
+    }
+}
