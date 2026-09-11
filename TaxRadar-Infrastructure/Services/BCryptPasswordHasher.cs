5a482d9bdc7e72011406b749aa07eb8345067d8c
Oleh_Hulevych <oleggulevic8@gmail.com>
2026-09-11 12:40:55 +0200

    added password hasher and verification

---
+++ TaxRadar-Infrastructure/Services/BCryptPasswordHasher.cs
@@ -0,0 +1,18 @@
+﻿using TaxRadar_Application.Interfaces;
+
+namespace TaxRadar_Infrastructure.Services;
+
+
+public class BCryptPasswordHasher:IPasswordHasher
+{
+    public string Hash(string password)
+    {
+        string passwordHash = BCrypt.Net.BCrypt.HashPassword(password);
+        return passwordHash;
+    }
+
+    public bool Verify(string password, string hash)
+    { 
+        return BCrypt.Net.BCrypt.Verify(password, hash);
+    }
+}
