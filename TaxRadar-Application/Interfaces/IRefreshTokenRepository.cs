fc7a1acb113a699a3601dbf26faf29a8b6d5e4bd
Oleh_Hulevych <oleggulevic8@gmail.com>
2026-09-12 12:10:05 +0200

    add repository for refreshTokens

---
+++ TaxRadar-Application/Interfaces/IRefreshTokenRepository.cs
@@ -0,0 +1,8 @@
+﻿using Tax_Radar_Domain.Entities;
+
+namespace TaxRadar_Application.Interfaces;
+
+public interface IRefreshTokenRepository:IRepository<RefreshToken>
+{
+    public Task<RefreshToken?> GetByUserId(Guid id, CancellationToken cancellationToken);
+}
---
+++ TaxRadar-Infrastructure/Repository/RefreshTokenRepository.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Tax_Radar_Domain.Entities;
+using TaxRadar_Application.Exceptions;
+using TaxRadar_Application.Interfaces;
+using TaxRadar_Infrastructure.Persistance;
+
+namespace TaxRadar_Infrastructure.Repository;
+
+public class RefreshTokenRepository(ApplicationDbContext context):IRefreshTokenRepository
+{
+    public async Task<RefreshToken?> GetByIdAsync(Guid id, CancellationToken cancellationToken)
+    {
+        var refreshToken = await context.RefreshTokens.FirstOrDefaultAsync(c=>c.Id==id,cancellationToken);
+        if (refreshToken == null) throw new NotFoundException(nameof(User), id);
+        return refreshToken;
+    }
+
+    public async Task AddAsync(RefreshToken entity, CancellationToken cancellationToken)
+    {
+        await context.RefreshTokens.AddAsync(entity, cancellationToken);
+        await SaveChangesAsync(cancellationToken);
+    }
+
+    public async Task DeleteAsync(RefreshToken entity, CancellationToken cancellationToken)
+    {
+        context.RefreshTokens.Remove(entity);
+        await context.SaveChangesAsync(cancellationToken);
+    }
+
+    public async Task SaveChangesAsync(CancellationToken cancellationToken)
+    {
+        await context.SaveChangesAsync(cancellationToken);
+    }
+
+    public async Task<RefreshToken?> GetByUserId(Guid id, CancellationToken cancellationToken)
+    {
+        return await context.RefreshTokens.Where(e=>e.UserId==id).FirstOrDefaultAsync(cancellationToken: cancellationToken);
+        
+    }
+}
