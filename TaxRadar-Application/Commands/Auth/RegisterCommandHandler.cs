be71b0a8a62014ea7ccd2d2c90385c3fb4cf7c5a
Oleh_Hulevych <oleggulevic8@gmail.com>
2026-09-12 17:02:36 +0200

    added command handler for registration

---
+++ TaxRadar-Application/Commands/Auth/RegisterCommandHandler.cs
@@ -0,0 +1,20 @@
+﻿using AutoMapper;
+using MediatR;
+using Tax_Radar_Domain.Entities;
+using TaxRadar_Application.DTOs.Users;
+using TaxRadar_Application.Interfaces;
+using TaxRadar_Application.Queries.Auth;
+
+namespace TaxRadar_Application.Commands.Auth;
+
+public class RegisterCommandHandler(IRepository<User> repository, IPasswordHasher passwordHasher, IMapper mapper):IRequestHandler<RegisterQuery, UserDto>
+{
+    public async Task<UserDto> Handle(RegisterQuery request, CancellationToken cancellationToken)
+    {
+        var hashedPassword = passwordHasher.Hash(request.Password);
+        var newUser = new User(request.Email,request.FullName,hashedPassword);
+        await repository.AddAsync(newUser, cancellationToken);
+        return mapper.Map<UserDto>(newUser);
+        
+    }
+}
--- TaxRadar-Application/Queries/Auth/RegisterQuery.cs
+++ TaxRadar-Application/Queries/Auth/RegisterQuery.cs
@@ -1,3 +1,6 @@
+using MediatR;
+using TaxRadar_Application.DTOs.Users;
+
 namespace TaxRadar_Application.Queries.Auth;
 
-public sealed record RegisterQuery(string Email, string FullName, string Password);
+public sealed record RegisterQuery(string Email, string FullName, string Password):IRequest<UserDto>;
